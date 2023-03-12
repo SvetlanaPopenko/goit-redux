09752500f8f9210313357c3595e16cea854a8872
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-12 04:20:19 +0200

    Update TaskList.js

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -8,7 +8,7 @@ const getVisibleTasks = (tasks,statusFilter) => {
   switch (statusFilter) {
     case statusFilters.active:
       return tasks.filter(task => !task.completed);
-    case statusFilter.completed:
+    case statusFilters.completed:
       return tasks.filter(task => task.completed);
     default:
       return tasks;
